f7280f0d3e8fd912091e0056bc88351ec595e476
TaraGithub <71233520+TaraGithub@users.noreply.github.com>
2023-04-30 15:58:38 -0500

    Updated App.java file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! from AWS-java" );
+        System.out.println( "Hello World from AWS-java and maven!" );
     }
 }
